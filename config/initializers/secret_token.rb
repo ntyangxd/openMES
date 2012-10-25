fa72aed0b01ae2dbe1c91b30db8216f7fb05becc
xiaodong yang <ntyangxd@gmail.com>
2012-10-25 14:10:15 +0800

    新建项目 Signed-off-by: xiaodong yang <ntyangxd@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenMES::Application.config.secret_token = 'a628d445da6a1df7f06f42dfb19cdc29330ef85c56b0c0b1050699a3d4aad3dbbbd53bb528e0cc44464900e35130f973451e133b251f9d1d27cac5f463da23eb'
